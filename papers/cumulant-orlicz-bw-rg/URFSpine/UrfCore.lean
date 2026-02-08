f72c251a718e90f46233fdd61ed300384c2c992d
inaciovasquez2020 <iinaciovasquez2020@gmail.com>
2026-02-08 15:23:44 -0300

    Ignore lake + LaTeX artifacts; add minimal URFSpine targets

---
+++ papers/cumulant-orlicz-bw-rg/URFSpine/UrfCore.lean
@@ -0,0 +1,3 @@
+namespace URFSpine
+
+end URFSpine
--- papers/cumulant-orlicz-bw-rg/lakefile.lean
+++ papers/cumulant-orlicz-bw-rg/lakefile.lean
@@ -2,7 +2,9 @@ import Lake
 open Lake DSL
 
 package cumulant_orlicz_bw_rg where
-  srcDir := "."
 
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
+@[default_target]
 lean_lib CumulantOrliczBwRg where
-  srcDir := "."
