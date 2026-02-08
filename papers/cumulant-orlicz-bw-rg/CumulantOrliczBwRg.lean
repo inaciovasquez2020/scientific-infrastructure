81b555cd6aad22cf3b989d42d54f787a8dace280
inaciovasquez2020 <iinaciovasquez2020@gmail.com>
2026-02-08 15:16:24 -0300

    Initialize Lake package for cumulant-orlicz-bw-rg

---
+++ papers/cumulant-orlicz-bw-rg/CumulantOrliczBwRg.lean
@@ -0,0 +1,3 @@
+namespace CumulantOrliczBwRg
+
+end CumulantOrliczBwRg
---
+++ papers/cumulant-orlicz-bw-rg/lakefile.lean
@@ -0,0 +1,8 @@
+import Lake
+open Lake DSL
+
+package cumulant_orlicz_bw_rg where
+  srcDir := "."
+
+lean_lib CumulantOrliczBwRg where
+  srcDir := "."
